018d11238ec685fed1bafae63f554ffefbe85ed1
Jimmy Phan <jphan9@gmail.com>
2019-04-28 15:02:14 -0700

    Added a new file for the math functionality

--- CSharpCalculator/Form1.cs
+++ CSharpCalculator/Form1.cs
@@ -16,7 +16,9 @@ namespace CSharpCalculator
         private double s2 = 0.0;
         private double total = 0.0;
         private Boolean addition, subtraction, division, multiplication, hasNumber = false;
-        private int count, firstCount = 0;
+        private int count, addCount, minusCount, divideCount, multiplyCount = 0;
+
+        mathFunctionality mathFunc = new mathFunctionality();
 
         public Form1()
         {
@@ -126,45 +128,74 @@ namespace CSharpCalculator
         private void acButton_Click(object sender, EventArgs e)
         {
             total = 0;
+            /*
             if (solutionTextBox.Text == "0" || solutionTextBox.Text == "" || hasNumber == true)
             {
                 return;
             }
             else
-                solutionTextBox.Text = "0";
+            */
+            solutionTextBox.Text = "0";
+
+            count = 0;
+            addCount = 0;
+            minusCount = 0;
+            divideCount = 0;
+            multiplyCount = 0;
+            hasNumber = false;
         }
 
         private void plusButton_Click(object sender, EventArgs e)
         {
             count++;
+            addCount++;
+            total = mathFunc.add(solutionTextBox.Text,addCount);
+            
+            /*            
             s1 = Double.Parse(solutionTextBox.Text);
-            solutionTextBox.Text = total.ToString();
-            total = s1;
+            //solutionTextBox.Text = total.ToString();
+
+            // If this is the first time that the button has been hit, set the total to the number in the text field. Else set the old total and divide by the number in the text field.
+            if (firstCount == 1)
+            {
+                total = s1;
+            }
+            else
+                total = total + s1;
             Console.WriteLine("plus s1: " + s1.ToString());
             Console.WriteLine("plus total: " + total.ToString());
+            */
             addition = true;
 
             solutionTextBox.Text = "";
 
-            /*if (count > 1)
+            if (count > 1)
             {
                 solutionTextBox.Text = total.ToString();
                 hasNumber = true;
             }
-            */
+
         }
 
         private void minusButton_Click(object sender, EventArgs e)
         {
-            if (total == 0)
+            minusCount++;
+            count++;
+
+            total = mathFunc.minus(solutionTextBox.Text, minusCount);
+
+            /*s1 = Double.Parse(solutionTextBox.Text);
+
+            // If this is the first time that the button has been hit, set the total to the number in the text field. Else set the old total and divide by the number in the text field.
+            if (firstCount == 1)
             {
                 total = s1;
             }
-            count++;
-            s1 = Double.Parse(solutionTextBox.Text);
-            total = total - s1;
-            Console.WriteLine(total.ToString());
+            else
+                total = total - s1;
+
             subtraction = true;
+            */
 
             solutionTextBox.Text = "";
 
@@ -178,17 +209,24 @@ namespace CSharpCalculator
         private void divideButton_Click(object sender, EventArgs e)
         {
             // Counter to check the first time hitting this button.
-            firstCount++;
+            divideCount++;
+
             // If the initial total amount is 0 then set the amount to s1.
+            /*
             if (total == 0)
             {
                 total = s1;
                 Console.WriteLine("here");
                 Console.WriteLine("First Total: " + total.ToString());
             }
+             * */
             count++;
+
+            total = mathFunc.divide(solutionTextBox.Text, divideCount);
+            /*
             s1 = Double.Parse(solutionTextBox.Text);
 
+            
             // If this is the first time that the button has been hit, set the total to the number in the text field. Else set the old total and divide by the number in the text field.
             if (firstCount == 1)
             {
@@ -199,40 +237,7 @@ namespace CSharpCalculator
             Console.WriteLine("Total: " + total.ToString());
             Console.WriteLine("S1: " + s1.ToString());
             division = true;
-
-            solutionTextBox.Text = "";
-
-            /*if (count > 1)
-            {
-                solutionTextBox.Text = total.ToString();
-                hasNumber = true;
-            }
-             * */
-        }
-
-        private void multiplicationButton_Click(object sender, EventArgs e)
-        {
-            firstCount++;
-            if (total == 0)
-            {
-                total = s1;
-                Console.WriteLine("here");
-                Console.WriteLine("First Total: " + total.ToString());
-            }
-            count++;
-            s1 = Double.Parse(solutionTextBox.Text);
-
-            // If this is the first time that the button has been hit, set the total to the number in the text field. Else set the old total and divide by the number in the text field.
-            if (firstCount == 1)
-            {
-                total = s1;
-            }
-            else
-                total = total / s1;
-            Console.WriteLine("Total: " + total.ToString());
-            Console.WriteLine("S1: " + s1.ToString());
-            multiplication = true;
-
+            */
             solutionTextBox.Text = "";
 
             if (count > 1)
@@ -242,8 +247,47 @@ namespace CSharpCalculator
             }
         }
 
+        private void multiplicationButton_Click(object sender, EventArgs e)
+        {
+            multiplyCount++;
+            /*
+            if (total == 0)
+            {
+                total = s1;
+                Console.WriteLine("here");
+                Console.WriteLine("First Total: " + total.ToString());
+            }
+             */
+
+            count++;
+            total = mathFunc.multiply(solutionTextBox.Text, multiplyCount);
+            /*
+            s1 = Double.Parse(solutionTextBox.Text);
+
+            // If this is the first time that the button has been hit, set the total to the number in the text field. Else set the old total and divide by the number in the text field.
+            if (firstCount == 1)
+            {
+                total = s1;
+            }
+            else
+                total = total * s1;
+            Console.WriteLine("Total: " + total.ToString());
+            Console.WriteLine("S1: " + s1.ToString());
+            multiplication = true;
+            */
+
+            solutionTextBox.Text = "";
+            
+            if (count > 1)
+            {
+                solutionTextBox.Text = total.ToString();
+                hasNumber = true;
+            }
+        }
+
         private void equalsButton_Click(object sender, EventArgs e)
         {
+            /*
             s2 = Double.Parse(solutionTextBox.Text);
             if (addition == true)
             {
@@ -269,12 +313,17 @@ namespace CSharpCalculator
                 Console.WriteLine("Real Total: " + total.ToString());
                 division = false;
             }
+            */
+
+            total = mathFunc.equals(solutionTextBox.Text);
             solutionTextBox.Text = total.ToString();
             Console.WriteLine("Text: " + solutionTextBox.Text);
             count = 0;
-            firstCount = 0;
+            addCount = 0;
+            minusCount = 0;
+            divideCount = 0;
+            multiplyCount = 0;
             hasNumber = false;
-            
         }
         
     }
---
+++ CSharpCalculator/mathFunctionality.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCalculator
+{
+    class mathFunctionality
+    {
+        public double firstNumber = 0.0;
+        public double secondNumber = 0.0;
+        public double total = 0.0;
+        public Boolean addition, subtraction, division, multiplication, hasNumber = false;
+        public int count, firstCount = 0;
+
+        public double add(string solution, int addCount)
+        {
+            firstNumber = Double.Parse(solution);
+            //solution = total.ToString();
+
+            // If this is the first time that the button has been hit, set the total to the number in the text field. Else set the old total and divide by the number in the text field.
+            if (addCount == 1)
+            {
+                total = firstNumber;
+            }
+            else
+                total = total + firstNumber;
+            Console.WriteLine("plus s1: " + firstNumber.ToString());
+            Console.WriteLine("plus total: " + total.ToString());
+            Console.WriteLine("firstCount : " + firstCount.ToString());
+            addition = true;
+
+            return total;
+            /*
+            solutionTextBox.Text = "";
+
+            if (count > 1)
+            {
+                solutionTextBox.Text = total.ToString();
+                hasNumber = true;
+            }
+             */
+        }
+
+        public double minus(string solution, int minusCount)
+        {
+            firstNumber = Double.Parse(solution);
+
+            // If this is the first time that the button has been hit, set the total to the number in the text field. Else set the old total and divide by the number in the text field.
+            if (minusCount == 1)
+            {
+                total = firstNumber;
+            }
+            else
+                total = total - firstNumber;
+
+            subtraction = true;
+
+            return total;
+        }
+
+        public double divide(string solution, int divideCount)
+        {
+            firstNumber = Double.Parse(solution);
+
+            // If this is the first time that the button has been hit, set the total to the number in the text field. Else set the old total and divide by the number in the text field.
+            if (divideCount == 1)
+            {
+                total = firstNumber;
+            }
+            else
+                total = total / firstNumber;
+            Console.WriteLine("Total: " + total.ToString());
+            Console.WriteLine("firstNumber: " + firstNumber.ToString());
+            division = true;
+
+            return total;
+        }
+
+        public double multiply(string solution, int multiplyCount)
+        {
+            firstNumber = Double.Parse(solution);
+
+            // If this is the first time that the button has been hit, set the total to the number in the text field. Else set the old total and divide by the number in the text field.
+            if (multiplyCount == 1)
+            {
+                total = firstNumber;
+            }
+            else
+                total = total * firstNumber;
+            Console.WriteLine("Total: " + total.ToString());
+            Console.WriteLine("firstNUmber: " + firstNumber.ToString());
+            multiplication = true;
+
+            return total;
+        }
+
+        public double equals(string solution)
+        {
+            secondNumber = Double.Parse(solution);
+            if (addition == true)
+            {
+                total = total + secondNumber;
+                Console.WriteLine("Addition S2: " + secondNumber.ToString());
+                Console.WriteLine("Addition Real Total: " + total.ToString());
+                addition = false;
+            }
+            else if (subtraction == true)
+            {
+                total = total - secondNumber;
+                subtraction = false;
+            }
+            else if (multiplication == true)
+            {
+                total = total * secondNumber;
+                multiplication = false;
+            }
+            else if (division == true)
+            {
+                total = total / secondNumber;
+                Console.WriteLine("S2: " + secondNumber.ToString());
+                Console.WriteLine("Real Total: " + total.ToString());
+                division = false;
+            }
+
+            return total;
+        }
+    }
+}
